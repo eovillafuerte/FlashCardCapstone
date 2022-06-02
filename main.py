bf53182962c6507f21d1624c1b86dc1f5d4b8eb6
eovillafuerte <eovillafuerte@outlook.com>
2022-06-01 18:39:30 -0600

    main.py update

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from tkinter import *
 import pandas
 import random
 
-BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
+to_learn = {}
+
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -21,6 +30,12 @@ def flip_card():
     canvas.itemconfig(card_title, text='English', fill='white')
     canvas.itemconfig(card_title, text=current_card['English'], fill='white')
     canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.Dataframe(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 window = Tk()
 window.title("Flashycardy")
@@ -42,7 +57,7 @@ unknown_button = Button(image=cross_image, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, command=next_card)
+known_button = Button(image=check_image, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
